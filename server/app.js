3acb5d3dacf4ff9a9dbe480105c853212502b9c2
Rahul Kumar <rahulkumar981417@gamil.com>
2024-01-03 10:12:22 +0530

    admin connection

--- server/app.js
+++ server/app.js
@@ -7,24 +7,39 @@ app.use(bodyParser.urlencoded({extended:true}));
 let connectionString = "mongodb://127.0.0.1:27017";
 app.use(bodyParser.json())
 app.use(cors());
+//default
 app.get('/',(req,res)=>{
     res.send({
         "name":"dav college",
         "project":"ems"
     })
 })
-// let formData={
-//     "username":"admin",
-//     "password":"admin"
-// }
+
 //admin-login
+
 app.post("/admin-login",(req,res)=>{
     let username = req.body.username;
     let password = req.body.password;
-    console.log(username);
-    console.log(password);
+  
+    mongoClient.connect(connectionString,(err,clientObject)=>{
+        if(!err){
+            let dbo = clientObject.db('DavEms');
+            dbo.collection('adminlogin').find({}).toArray((err,documents)=>{
+                if(!err){
+                    if(username==documents[0].Admin && password==documents[0].Password){
+                        res.status(200)
+                      console.log("login success")  
+                    }else{
+                        console.log("data does not match");
+                    }
+                    
+                }
+            })
+        }
+    })
     res.send("data received");
 }) 
+
 //admin-dashboard api
 app.post('/admin-dashboard',(req,res)=>{
      let department = req.body.department;
@@ -50,6 +65,8 @@ app.post('/admin-dashboard',(req,res)=>{
     })
      res.send("data received successfully");
 })
+
+   //get data
     app.get('/getdata',(req,res)=>{
         mongoClient.connect(connectionString,(err,clientObject)=>{
             if(!err){
@@ -62,5 +79,37 @@ app.post('/admin-dashboard',(req,res)=>{
             }
         })
     })
+    app.post('/adddepartment',function(req,res){
+       let department= req.body.department;
+       let data = {
+        "department":department
+       }
+       console.log(department);
+       mongoClient.connect(connectionString,(err,clientObject)=>{
+        if(!err){
+            let dbo = clientObject.db('DavEms');
+            dbo.collection('department').insertOne(data,(err,result)=>{
+                if(!err){
+                    console.log("record inserted")
+                }
+            })
+        
+        }
+    })
+    res.send("data inserted");
+    })
+    // app.get('/admin/login',(req,res)=>{
+        
+    //         mongoClient.connect(connectionString,(err,clientObject)=>{
+    //             if(!err){
+    //                 let dbo = clientObject.db('DavEms');
+    //                 dbo.collection('adminlogin').find({}).toArray((err,documents)=>{
+    //                     if(!err){
+    //                        res.send(documents)
+    //                     }
+    //                 })
+    //             }
+    //         })
+    // })
 app.listen(9090)
 console.log("server started")
